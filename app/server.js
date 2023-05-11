b464d23f323da870b8efed76fcd1afd48134e580
BranBao1995 <brandon.0702@hotmail.com>
2023-05-11 14:40:17 -0400

    added my-app image to services

--- app/server.js
+++ app/server.js
@@ -40,7 +40,7 @@ app.post("/update-profile", function (req, res) {
   let userObj = req.body;
 
   MongoClient.connect(
-    mongoUrlLocal,
+    mongoUrlDocker,
     mongoClientOptions,
     function (err, client) {
       if (err) throw err;
@@ -70,7 +70,7 @@ app.get("/get-profile", function (req, res) {
   let response = {};
   // Connect to the db
   MongoClient.connect(
-    mongoUrlLocal,
+    mongoUrlDocker,
     mongoClientOptions,
     function (err, client) {
       if (err) throw err;
